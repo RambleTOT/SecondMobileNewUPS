f516fd381f0afa1b95eae02d4cbaa4aefc5c91ec
RambleTOT <90986878+RambleTOT@users.noreply.github.com>
2023-10-25 03:26:42 +0500

    initial commit

---
+++ app/src/main/java/ramble/sokol/secondnewups/auth/view/StartScreen.kt
@@ -0,0 +1,10 @@
+package ramble.sokol.secondnewups.auth.view
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination(start = true)
+fun StartScreen(){
+
+}
---
+++ app/src/main/java/ramble/sokol/secondnewups/main/MainActivity.kt
@@ -0,0 +1,24 @@
+package ramble.sokol.secondnewups.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ramcosta.composedestinations.DestinationsNavHost
+import ramble.sokol.secondnewups.auth.view.NavGraphs
+import ramble.sokol.secondnewups.ui.theme.SecondNewUPSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DestinationsNavHost(navGraph = NavGraphs.root)
+        }
+    }
+}
